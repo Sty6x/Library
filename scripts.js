c2e2603858530526de61038d50c31876d7b61ef6
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-07-15 10:42:00 +0400

    added pop up card

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 let library = []
 
-function Book(title, author, pages) {
+function Book(title, author, pages,description) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.description = description
 }
 
-function addBookToLibrary(title, author, pages) {
-    let book = new Book(title, author, pages)
+function addBookToLibrary(title, author, pages ,description) {
+    let book = new Book(title, author, pages,description)
     library.push(book)
     return book;
 }
@@ -43,18 +44,32 @@ function showBook(library) {
 const addButton =  document.getElementById('add');
 addButton.addEventListener('click', () => {
     console.log('book is added!')
-    addBookToLibrary(title.value,author.value,pages.value)
+    addBookToLibrary(title.value,author.value,pages.value,description.value)
     
     // resets value after every add press to prevent
     // duplication everytime button is pressed
     title.value = '';
     author.value = '';
     pages.value = '';
+    description.value = '';
 })
 
 const displayBook = document.getElementById('display-books')
 displayBook.addEventListener('click' , ()=>{
     console.log('book is being displayed!')
     showBook(library)
-    console.log(library)
+    // css display set to none after adding
+    popUpFrom.style.display = 'none'
 })
+
+
+
+
+
+const popUpFrom = document.querySelector('.pop-up-card');
+const addButtonIcon = document.querySelector('.add-book-icon');
+addButtonIcon.addEventListener('click', ()=>{
+    popUpFrom.style.display = 'flex'
+})
+
+
